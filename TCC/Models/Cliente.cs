9ac92d9c00092ddd314aef6cbe788ca730094483
CaioAbbes <caio.abbes.pereira@outlook.com>
2020-10-20 18:41:33 -0300

    Colocando a classe de conexao e criando os metodos do Cliente

--- TCC/Models/Cliente.cs
+++ TCC/Models/Cliente.cs
@@ -8,6 +8,9 @@ namespace TCC.Models
 {
     public class Cliente
     {
+        private ConexaoDB db = new ConexaoDB();
+
+
         [Required(ErrorMessage = "O campo Id do cliente é requerido.")]
         [Display(Name = "Id do cliente")]
         [RegularExpression(@"^\d+$", ErrorMessage = "Digite somente números.")]
@@ -23,6 +26,35 @@ namespace TCC.Models
         [Display(Name = "CEP")]
         [RegularExpression(@"^[0-9]{5}-[\d]{3}|(\d{8})$", ErrorMessage = "CEP invalido.")]
         public decimal CEP { get; set; }
+
+        [Required(ErrorMessage = "O campo Logradouro é requerido.")]
+        [Display(Name = "Logradouro")]
+        [RegularExpression(@"^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ'\s]+$", ErrorMessage = "Digite somente letras.")]
+        [StringLength(200, ErrorMessage = "A quantidade de caracteres do Logradouro é invalido.")]
+        public string Logra { get; set; }
+
+        [Required(ErrorMessage = "O campo Bairro é requerido.")]
+        [Display(Name = "Bairro")]
+        [RegularExpression(@"^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ'\s]+$", ErrorMessage = "Digite somente letras.")]
+        [StringLength(200, ErrorMessage = "A quantidade de caracteres do Bairro é invalido.")]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "O campo Cidade é requerido.")]
+        [Display(Name = "Cidade")]
+        [RegularExpression(@"^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ'\s]+$", ErrorMessage = "Digite somente letras.")]
+        [StringLength(100, ErrorMessage = "A quantidade de caracteres do Cidade é invalido.")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "O campo Estado é requerido.")]
+        [Display(Name = "Estado")]
+        [RegularExpression(@"^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ'\s]+$", ErrorMessage = "Digite somente letras.")]
+        [StringLength(100, ErrorMessage = "A quantidade de caracteres do Estado é invalido.")]
+        public string Estado { get; set; }
+
+        [Required(ErrorMessage = "O campo UF é requerido.")]
+        [Display(Name = "UF")]
+        [RegularExpression(@"^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ'\s]+$", ErrorMessage = "Digite somente letras.")]
+        public char UF { get; set; }
 
         [Required(ErrorMessage = "O campo Nome do cliente é requerido.")]
         [Display(Name = "Nome do cliente")]
@@ -57,12 +89,35 @@ namespace TCC.Models
         [Range(0, int.MaxValue, ErrorMessage = "Deve ser positivo")]
         public int IdUsuario { get; set; }
 
+        [Required(ErrorMessage = "O campo Usuário é requerido.")]
+        [Display(Name = "Usuário")]
+        public string UsuarioText { get; set; }
+
+        [Required(ErrorMessage = "O campo Senha é requerido.")]
+        [Display(Name = "Senha")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
         [Range(0,float.MaxValue,ErrorMessage = "Deve ser positivo")]
         [Display(Name = "Quantidade de pontos")]
         [RegularExpression(@"^\d+$", ErrorMessage = "Digite somente números.")]
         public float QtdPontos { get; set; }
 
 
+
+
      //   public HttpPostedFileBase Imagecli { get; set; } //ou string
+
+
+
+        public void InsertCliente(Cliente cliente)
+        {
+            string strQuery = string.Format("CALL sp_InsEnderecoCliUsu ('{0}','{1}','{2}','{3}',{4},'{5}','{6}','{7}',{8},'{9}',{10},'{11}',{12},'{13}';",cliente.UsuarioText,cliente.Senha,cliente.UF,cliente.Cidade,cliente.CEP,cliente.Logra,cliente.Bairro,cliente.Comp,cliente.NumEdif,cliente.NomeCli,cliente.CPF,cliente.EmailCli,cliente.CelCli,cliente.Estado);
+
+            using (db = new ConexaoDB())
+            {
+                db.ExecutaComando(strQuery);
+            }
+        }
     }
 }
---
+++ TCC/Models/ConexaoDB.cs
@@ -0,0 +1,74 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace TCC.Models
+{
+    public class ConexaoDB : IDisposable
+    {
+        private readonly MySqlConnection conexao;
+
+        public ConexaoDB()
+        {
+            conexao = new MySqlConnection(ConfigurationManager.ConnectionStrings["conexao"].ConnectionString);
+            conexao.Open();
+        }
+
+        public void ExecutaComando(string StrQuery)
+        {
+            var vComando = new MySqlCommand
+            {
+
+                CommandText = StrQuery,
+                CommandType = System.Data.CommandType.Text,
+                Connection = conexao
+            };
+            vComando.ExecuteNonQuery();
+        }
+
+        public MySqlDataReader ExecutaRegistro(string StrQuery)
+        {
+            var vComando = new MySqlCommand
+            {
+
+                CommandText = StrQuery,
+                CommandType = System.Data.CommandType.Text,
+                Connection = conexao
+            };
+            return vComando.ExecuteReader();
+        }
+
+        public string ExecutaDado(string StrQuery)
+        {
+            var vComando = new MySqlCommand
+            {
+
+                CommandText = StrQuery,
+                CommandType = System.Data.CommandType.Text,
+                Connection = conexao
+            };
+
+            string result;
+            result = (string)vComando.ExecuteScalar();
+
+            if (result == null)
+            {
+                return result = "";
+                // ou esse -> return "";
+            }
+
+            return result;
+        }
+
+        public void Dispose()
+        {
+            if (conexao.State == System.Data.ConnectionState.Open)
+            {
+                conexao.Close();
+            }
+        }
+    }
+}
