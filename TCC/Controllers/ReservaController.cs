53a7d2770a5310be98258cd8a67e59d4e855a94f
CaioAbbes <caio.abbes.pereira@outlook.com>
2020-11-13 15:33:52 -0300

    Fazendo o CRUD da Reserva

---
+++ TCC/Controllers/ReservaController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TCC.Models;
+
+namespace TCC.Controllers
+{
+    public class ReservaController : Controller
+    {
+        // GET: Reserva
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Reserva/Details/5
+        public ActionResult Details(int IdReserva)
+        {
+            var Reserva = new Reserva();
+            var objReserva = new Reserva();
+            Reserva = objReserva.SelecionaIdReserva(IdReserva);
+            return View(Reserva);
+        }
+
+        public ActionResult List(Reserva reserva)
+        {
+            var reservaList = reserva.SelecionaReserva();
+
+            var objReserva = new Reserva();
+            objReserva.SelecionaReserva();
+
+            return View(reservaList);
+        }
+
+        // GET: Reserva/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reserva/Create
+        [HttpPost]
+        public ActionResult Create(Reserva reserva)
+        {
+            if (ModelState.IsValid)
+            {
+                var objReserva = new Reserva();
+                objReserva.InsertReserva(reserva);
+                return RedirectToAction("List");
+            }
+
+            return View();
+        }
+
+        // GET: Reserva/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Reserva/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reserva/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Reserva/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- TCC/Models/Reserva.cs
+++ TCC/Models/Reserva.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,32 +9,89 @@ namespace TCC.Models
 {
     public class Reserva
     {
+        private ConexaoDB db;
+
         [Required(ErrorMessage = "O campo Id da reserva é requerido.")]
         [Display(Name = "Id da reserva")]
-        [RegularExpression(@"^\d+$", ErrorMessage = "Digite somente números.")]
         [Range(0, int.MaxValue, ErrorMessage = "Deve ser positivo")]
         public int IdReserva { get; set; }
 
-        [Display(Name = "Id da mesa")]
-        [RegularExpression(@"^\d+$", ErrorMessage = "Digite somente números.")]
-        [Range(0, int.MaxValue, ErrorMessage = "Deve ser positivo")]
-        public int IdMesa { get; set; }
+        //[Display(Name = "Id da mesa")]
+        //[Range(0, int.MaxValue, ErrorMessage = "Deve ser positivo")]
+        //public int IdMesa { get; set; }
 
-        [Required(ErrorMessage = "O campo CPF é requerido.")]
-        [Display(Name = "CPF")]
-        [RegularExpression(@"^(\d{3}.\d{3}.\d{3}-\d{2})|(\d{11})$", ErrorMessage = "CPF invalido.")]
-        public decimal CPF { get; set; }
+        public Mesa Mesa { get; set; }
+
+        //[Required(ErrorMessage = "O campo Id do Cliente é requerido.")]
+        //[Display(Name = "Id do Cliente")]
+        //public decimal IdCli { get; set; }
+
+        public Cliente Cliente { get; set; }
 
         [Required(ErrorMessage = "O campo Data e hora da reserva é requerido.")]
         [Display(Name = "Data e hora da reserva")]
-        [DisplayFormat(DataFormatString = "mm/dd/yyyy")]
         [DataType(DataType.DateTime)]
         public DateTime DataHoraReserva { get; set; }
 
-        [Required(ErrorMessage = "O campo Data e hora que quero come é requerido.")]
-        [Display(Name = "Data e hora que quero comer")]
-        [DisplayFormat(DataFormatString = "mm/dd/yyyy")]
-        [DataType(DataType.DateTime)]
-        public DateTime HoraQueroComer { get; set; }
+        //[Required(ErrorMessage = "O campo Data e hora que quero come é requerido.")]
+        //[Display(Name = "Data e hora que quero comer")]
+        //[DataType(DataType.DateTime)]
+        //public DateTime HoraQueroComer { get; set; }
+
+        public void InsertReserva(Reserva reserva)
+        {
+            string strQuery = string.Format("call sp_InsReserva('{0}','{1}','{2}','{3}');", 1,reserva.DataHoraReserva.ToString("yyyy-MM-dd"),reserva.Mesa.Numlugares,reserva.Mesa.TipoLugar);
+
+            using (db = new ConexaoDB())
+            {
+                db.ExecutaComando(strQuery);
+            }
+        }
+
+        public List<Reserva> SelecionaReserva()
+        {
+            using (db = new ConexaoDB())
+            {
+                string StrQuery = string.Format("select * from tbreserva;");
+                MySqlDataReader registros = db.RetornaRegistro(StrQuery);
+                var reservaList = new List<Reserva>();
+                while (registros.Read())
+                {
+                    var ReservaTemporaria = new Reserva
+                    {
+                        IdReserva = int.Parse(registros["IdReserva"].ToString()),
+                        Mesa = new Mesa().SelecionaIdMesa(int.Parse(registros["IdMesa"].ToString())),
+                        Cliente = new Cliente().SelecionaIdCli(int.Parse(registros["IdCli"].ToString())),
+                        DataHoraReserva = DateTime.Parse(registros["DataHoraReserva"].ToString()),
+                        //HoraQueroComer = DateTime.Parse(registros["DataQueroComer"].ToString())
+                    };
+                    reservaList.Add(ReservaTemporaria);
+                }
+                return reservaList;
+            }
+        }
+
+        public Reserva SelecionaIdReserva(int IdReserva)
+        {
+            using (db = new ConexaoDB())
+            {
+                string StrQuery = string.Format("select * from tbreserva where IdReserva = '{0}';", IdReserva);
+                MySqlDataReader registros = db.RetornaRegistro(StrQuery);
+                Reserva reservaListando = null;
+                while (registros.Read())
+                {
+                    reservaListando = new Reserva
+                    {
+                        IdReserva = int.Parse(registros["IdReserva"].ToString()),
+                        Mesa = new Mesa().SelecionaIdMesa(int.Parse(registros["IdMesa"].ToString())),
+                        Cliente = new Cliente().SelecionaIdCli(int.Parse(registros["IdCli"].ToString())),
+                        DataHoraReserva = DateTime.Parse(registros["DataHoraReserva"].ToString()),
+                    };
+                }
+
+                return reservaListando;
+            }
+
+        }
     }
 }
